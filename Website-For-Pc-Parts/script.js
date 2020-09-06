b2ce8bdb43b0310441054e70cefe367e02bca602
Website1Creator <61760572+Website1Creator@users.noreply.github.com>
2020-09-07 10:21:51 +1200

    Nav Done
    NAV almost finished needs links

---
+++ Website-For-Pc-Parts/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+	document.getElementById("nav").style.height = "20%";
+	document.getElementById("main").style.marginleft = "20%";
+	}
+
+function closeNav() {
+	document.getElementById("nav").style.height = "0%";
+	document.getElementById("main").style.marginleft = "0%";
+	}
