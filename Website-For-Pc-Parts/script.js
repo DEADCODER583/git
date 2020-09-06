0217b93c412da5bcd38ea3c618df8083dff11bb5
Website1Creator <61760572+Website1Creator@users.noreply.github.com>
2020-09-07 10:33:19 +1200

    Delete script.js

--- Website-For-Pc-Parts/script.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-	document.getElementById("nav").style.height = "20%";
-	document.getElementById("main").style.marginleft = "20%";
-	}
-
-function closeNav() {
-	document.getElementById("nav").style.height = "0%";
-	document.getElementById("main").style.marginleft = "0%";
-	}
