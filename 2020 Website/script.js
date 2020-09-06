fc1fd47911539ad179188b02b67d2fbce45d64eb
Website1Creator <61760572+Website1Creator@users.noreply.github.com>
2020-09-07 10:35:24 +1200

    Nav Js And Styling Done Code CLeaned

---
+++ 2020 Website/script.js
@@ -0,0 +1,11 @@
+console.log("HIIIIII")
+function openNav() {
+    document.getElementById("nav").style.height = "20%";
+    
+    }
+	
+function closeNav() {
+
+    document.getElementById("nav").style.height = "0%";
+	
+	}
